11553665ca89799917eb765fcf744a552b5fb610
ALIAMINE632 <benaliamine632@gmail.com>
2025-08-23 17:20:33 +0100

    une modification simple pour demo

--- amineali/essai.py
+++ amineali/essai.py
@@ -1,1 +1,8 @@
 print("juste un essai")
+print("hello world")
+for i in range(5):
+    print(i)
+    print("hello collabotoratory")
+    print("collaboration is the key to success")
+    print("let's work together")
+    
